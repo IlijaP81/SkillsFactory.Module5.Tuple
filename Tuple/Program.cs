f0e581f0c8b114b5b02ea870ae1445587b2fca61
IlijaP81 <niilp1999no@gmail.com>
2024-08-05 10:52:24 +0300

    Добавьте файлы проекта.

---
+++ Tuple/Program.cs
@@ -0,0 +1,17 @@
+﻿class MainClass
+{
+    public static void Main(string[] args)
+    {
+        (string Name, string[] Dishes) User;
+        User.Dishes = new string[5];
+        Console.WriteLine("Введите имя:");
+        User.Name = Console.ReadLine();
+        
+        for (int i = 0; i < User.Dishes.Length; i++)
+        {
+            Console.WriteLine($"Укажите название {i + 1}-го любимого блюда:");
+            User.Dishes[i] = Console.ReadLine();
+        }
+        Console.ReadKey();
+    }
+}
